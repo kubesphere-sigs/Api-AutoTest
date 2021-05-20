daf30f153116c4c9bde3e06d93232eb2bc762276
wenxin <stevewen@yunify.com>
2021-05-20 14:54:57 +0800

    优化token获取

--- common/getCookie.py
+++ common/getCookie.py
@@ -1,24 +1,24 @@
 import requests
+from config import config
 
 
 def get_token(server):
-    header = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Connection': 'close'
-    }
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64)", "Connection": "close"}
     data = {
         'username': 'admin',
         'password': 'P@88w0rd',
         'grant_type': 'password'
     }
-
-    r = requests.post(server + '/oauth/token', headers=header, data=data)
-
-    if r.status_code == 200:
-        token = r.json()['access_token']
-    else:
-        # logger.info('get token response:{}'.format(r))
-        raise Exception('get token failed!')
+    try:
+        r = requests.post(server + '/oauth/token', headers=header, data=data)
+        if r.status_code == 200:
+            token = r.json()['access_token']
+        else:
+            # logger.info('get token response:{}'.format(r))
+            raise Exception('get token failed!')
+    except requests.exceptions.ConnectionError as e:
+        print('Error', e.args)
 
     ks_token = 'Bearer ' + token
     return ks_token
+
