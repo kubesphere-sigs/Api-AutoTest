1735f0645da424ae4beb9e658bf23d3aa30e5446
wenxin <stevewen@yunify.com>
2021-05-31 15:25:53 +0800

    增加访问帮助文档的测试用例

---
+++ TestCase/testHelp.py
@@ -0,0 +1,60 @@
+import requests
+import pytest
+import allure
+import sys
+
+sys.path.append('../')  # 将项目路径加到搜索路径中，使得自定义模块可以引用
+
+from common.getData import DoexcleByPandas
+
+
+@allure.feature('Help')
+class TestHelp(object):
+    # 从文件中读取用例信息
+    parametrize = DoexcleByPandas().get_data_for_pytest(filename='../data/data.xlsx', sheet_name='help')
+
+    @allure.title('{title}')  # 设置用例标题
+    @allure.severity(allure.severity_level.CRITICAL)  # 设置用例优先级
+    # 将用例信息以参数化的方式传入测试方法
+    @pytest.mark.parametrize('id,url,data,story,title,method,severity,condition,except_result', parametrize)
+    def test_help(self, id, url, data, story, title, method, severity, condition, except_result):
+
+        """
+        :param id: 用例编号
+        :param url: 用例请求的URL地址
+        :param data: 用例使用的请求数据
+        :param story: 用例模块
+        :param title: 用例标题
+        :param method: 用例的请求方式
+        :param severity: 用例优先级
+        :param condition: 用例的校验条件
+        :param except_result: 用例的预期结果
+        """
+
+        allure.dynamic.story(story)  # 动态生成模块
+        allure.dynamic.severity(severity)  # 动态生成用例等级
+        # 测试get方法
+        r = requests.get(url)
+        # 将校验条件和预期结果参数化
+        if condition != 'nan':
+            condition_new = eval(condition)  # 将字符串转化为表达式
+            if isinstance(condition_new, str):
+                # 判断表达式的结果是否为字符串，如果为字符串格式，则去掉其首尾的空格
+                assert condition_new.strip() == except_result
+            else:
+                assert condition_new == except_result
+
+        # 将用例中的内容打印在报告中
+        print(
+            '用例编号: ' + str(id) + '\n'
+                                 '用例请求的URL地址: ' + str(url) + '\n'
+                                                             '用例使用的请求数据: ' + str(data) + '\n'
+                                                                                         '用例模块: ' + story + '\n'
+                                                                                                            '用例标题: ' + title + '\n'
+                                                                                                                               '用例的请求方式: ' + method + '\n'
+                                                                                                                                                      '用例优先级: ' + severity + '\n'
+                                                                                                                                                                             '用例的校验条件: ' + str(
+                condition) + '\n'
+                             '用例的实际结果: ' + str(condition_new) + '\n'
+                                                                '用例的预期结果: ' + str(except_result)
+        )
