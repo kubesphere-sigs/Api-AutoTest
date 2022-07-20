de91a41e3de848cff40ef0969df91e97be9338cf
wenxin <568007213@qq.com>
2022-07-20 09:56:02 +0800

    添加路径存在与否的判断

--- common/getConfig.py
+++ common/getConfig.py
@@ -1,15 +1,20 @@
 import yaml
 import sys
+import os
 
 sys.path.append('../')  # 将项目路径加到搜索路径中，使得自定义模块可以引用
 
 # 获取yaml文件路径
 yamlPath = '../config/config_new.yaml'
+yamlPath_1 = 'config/config_new.yaml'
 
 
 def get_apiserver():
     # open方法打开直接读出来
-    f = open(yamlPath, 'r', encoding='utf-8')
+    if os.path.exists(yamlPath):
+        f = open(yamlPath, 'r', encoding='utf-8')
+    else:
+        f = open(yamlPath_1, 'r', encoding='utf-8')
     cfg = f.read()
     # print(type(cfg))  # 读出来是字符串
     # print(cfg)
