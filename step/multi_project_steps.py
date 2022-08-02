c15d671aa4cb4c4935841ce85d72a7687bb02525
wenxin <568007213@qq.com>
2022-08-02 09:25:35 +0800

    多集群环境项目步骤调整

--- step/multi_project_steps.py
+++ step/multi_project_steps.py
@@ -826,3 +826,20 @@ def step_delete_project(cluster_name, ws_name, project_name):
     return response
 
 
+@allure.step('在指定集群创建项目')
+def step_create_project_for_cluster(cluster_name, ws_name, project_name):
+    url = env_url + '/kapis/clusters/' + cluster_name + '/tenant.kubesphere.io/v1alpha2/workspaces/' + \
+          ws_name + '/namespaces'
+    data = {"apiVersion": "v1", "kind": "Namespace",
+            "metadata": {"name": project_name, "labels": {"kubesphere.io/workspace": ws_name},
+                         "annotations": {"kubesphere.io/creator": "admin"}}, "cluster": cluster_name}
+    response = requests.post(url=url, headers=get_header(), data=json.dumps(data))
+    return response
+
+
+@allure.step('从指定集群删除项目')
+def step_delete_project_from_cluster(cluster_name, ws_name, project_name):
+    url = env_url + '/kapis/clusters/' + cluster_name + '/tenant.kubesphere.io/v1alpha2/workspaces/' + \
+          ws_name + '/namespaces/' + project_name
+    response = requests.delete(url=url, headers=get_header())
+    return response
