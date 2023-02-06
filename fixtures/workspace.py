f454779308a3b0b0d8ae2ab311cc8b88012d3d11
wenxin-01 <568007213@qq.com>
2023-02-06 15:07:32 +0800

    修改用例步骤

--- fixtures/workspace.py
+++ fixtures/workspace.py
@@ -1,6 +1,6 @@
 import pytest
 from common import commonFunction
-from step import workspace_steps, cluster_steps, multi_project_steps
+from step import multi_cluster_steps, cluster_steps, multi_project_steps, multi_workspace_steps
 
 
 @pytest.fixture
@@ -10,9 +10,9 @@ def create_multi_workspace():
     alias_name = '跨集群'
     description = '包含所有的集群的企业空间'
     clusters = cluster_steps.step_get_cluster_name()
-    workspace_steps.step_create_multi_ws(ws_name, alias_name, description, clusters)
+    multi_workspace_steps.step_create_multi_ws(ws_name, alias_name, description, clusters)
     yield ws_name
-    workspace_steps.step_delete_workspace(ws_name)
+    multi_workspace_steps.step_delete_workspace(ws_name)
 
 
 @pytest.fixture
